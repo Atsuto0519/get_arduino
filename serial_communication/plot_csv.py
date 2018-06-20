52580a8d77159dab8e2c6fa06f773dc3330511ec
Astuto0519 <atsuto.fps@gmail.com>
2018-06-20 20:13:17 +0900

    Add newfile and modified

---
+++ serial_communication/plot_csv.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+openfile = '2018-06-20_1.csv'
+savefig = '2018-06-20_1.png'
+
+df = pd.read_csv(openfile)
+
+# time列で重複した行を、初めの行のみにたたみ込む
+# df_zip = df.drop_duplicates(subset="time")
+df_zip = df
+df_zip.time = pd.to_datetime(df_zip.time)
+df_zip.temperature = df_zip.temperature.convert_objects(convert_numeric=True)
+
+mean = df_zip['temperature'].mean()
+var = df_zip['temperature'].var(ddof=False)
+
+print(type(df_zip.loc[0, 'time']))
+
+df_zip = df_zip.set_index('time')
+
+df_zip.plot()
+plt.grid()
+# 1σ（全体の68%）が収まるようにしてみた
+plt.ylim(mean-0.5*var, mean+0.5*var)
+plt.savefig(savefig)
--- serial_communication/serial_to_csv.py
+++ serial_communication/serial_to_csv.py
@@ -1,14 +1,14 @@
 # Arduinoシリアルコンソールからcsvにデータを落とし込む
 
+# 必要なライブラリインポート
 import serial
 import serial.tools.list_ports
+import os
 import csv
+import datetime
 
-# 出力CSVファイル名
-savefile = 'data1500.csv'
-
-# MacでArduinoシリアルコンソールをCSVに落とし込むとき
-def get_serial_for_mac() :
+# MacでArduinoシリアルコンソールのコムポート自動決定
+def get_port_for_mac() :
     ser = serial.Serial()
     ser.baudrate = 9600
     for file in os.listdir('/dev'):
@@ -17,5 +17,59 @@ def get_serial_for_mac() :
             # ser.portにCOM?の情報が入る
             ser.port = '/dev/'+file
 
+            return ser
+
+# Windowsなら
+def get_port_for_win() :
+    ser = serial.Serial()
+    ser.baudrate = 9600
+    for i in range(100):
+        try:
+            ser.port = i
+            ser.open()
+
+            return ser
+
+        except:
+            i = i
+
+
+# 出力CSVファイル名
+savefile = str(datetime.date.today())+'_1.csv'
 sf = open(savefile, 'w')
 csvWriter = csv.writer(sf)
+
+# 個別指定
+# Macなら、ターミナル→'ls /dev'
+# →出てきたそれっぽいやつ（/dev/tty.usbmodem）をser.portへ
+ser = serial.Serial()
+ser.baudrate = 9600
+ser.port = '/dev/tty.usbmodem1411'
+
+# 自動ポート指定
+# ser = get_port_for_mac()
+
+# 接続
+ser.open()
+
+# CSVのヘッダー設定
+csvWriter.writerow(['time','temperature'])
+
+# シリアルストリート処理部分
+while 1 :
+    line = ser.readline()
+    data = str(line)
+
+    if "'" in data :
+        data = data.split("'")[1]
+
+    if "\\" in data :
+        data = data.split("\\")[0]
+
+    if ":" in data :
+        data = data.split(":")[-1]
+
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+","+data)
+    csvWriter.writerow([datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),data])
+
+ser.close()
